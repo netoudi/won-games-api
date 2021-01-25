ca10eac68b936d2121cd22b02f20e85a88091c9e
netoudi <netoudi@rocketmail.com>
2021-01-25 11:00:00 -0300

    Refactor LeftMenuHeader styles

--- admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
+++ admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
@@ -4,24 +4,12 @@ import Logo from '../../../assets/images/logo-won.svg';
 
 const Wrapper = styled.div`
   ${({ theme }) => css`
-    background-color: #007eff;
-    padding-left: 2rem;
+    background-color: ${theme.main.colors.won.blue};
     height: ${theme.main.sizes.leftMenu.height};
-
-    .leftMenuHeaderLink {
-      &:hover {
-        text-decoration: none;
-      }
-    }
 
     .projectName {
       display: block;
-      width: 100%;
       height: ${theme.main.sizes.leftMenu.height};
-      font-size: 2rem;
-      letter-spacing: 0.2rem;
-      color: $white;
-
       background-image: url(${Logo});
       background-repeat: no-repeat;
       background-position: center center;

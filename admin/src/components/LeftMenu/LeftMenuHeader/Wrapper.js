6568d906244671ed5e1baac02ef2686c157fa364
netoudi <netoudi@rocketmail.com>
2021-01-25 10:49:52 -0300

    Change Logo Admin

---
+++ admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
@@ -0,0 +1,33 @@
+import styled, { css } from 'styled-components';
+
+import Logo from '../../../assets/images/logo-won.svg';
+
+const Wrapper = styled.div`
+  ${({ theme }) => css`
+    background-color: #007eff;
+    padding-left: 2rem;
+    height: ${theme.main.sizes.leftMenu.height};
+
+    .leftMenuHeaderLink {
+      &:hover {
+        text-decoration: none;
+      }
+    }
+
+    .projectName {
+      display: block;
+      width: 100%;
+      height: ${theme.main.sizes.leftMenu.height};
+      font-size: 2rem;
+      letter-spacing: 0.2rem;
+      color: $white;
+
+      background-image: url(${Logo});
+      background-repeat: no-repeat;
+      background-position: center center;
+      background-size: 12rem;
+    }
+  `}
+`;
+
+export default Wrapper;

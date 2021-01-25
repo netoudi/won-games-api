12e665d957184afeb63f1cf063845e9e2f6b136f
netoudi <netoudi@rocketmail.com>
2021-01-25 16:07:00 -0300

    Find by existent data

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -24,6 +24,22 @@ async function getGameInfo(slug) {
   };
 }
 
+async function getByName(name, entityName) {
+  const item = await strapi.services[entityName].find({ name });
+  return item.length ? item[0] : null;
+}
+
+async function create(name, entityName) {
+  const item = await getByName(name, entityName);
+
+  if (!item) {
+    return await strapi.services[entityName].create({
+      name,
+      slug: slugify(name, { lower: true, strict: true }),
+    });
+  }
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -32,15 +48,8 @@ module.exports = {
 
     console.log(products[0]);
 
-    await strapi.services.publisher.create({
-      name: products[0].publisher,
-      slug: slugify(products[0].publisher, { lower: true, strict: true }),
-    });
-
-    await strapi.services.developer.create({
-      name: products[0].developer,
-      slug: slugify(products[0].developer, { lower: true, strict: true }),
-    });
+    await create(products[0].publisher, 'publisher');
+    await create(products[0].developer, 'developer');
 
     // console.log(await getGameInfo(products[0].slug));
   },

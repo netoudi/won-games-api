728aafefd6c747588967e87585a7c8fb5010b036
netoudi <netoudi@rocketmail.com>
2021-03-17 15:40:57 -0300

    Add db connection production

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: env('NODE_ENV') === 'production'
+    ? 'production'
+    : 'default',
   connections: {
     default: {
       connector: 'bookshelf',
@@ -10,9 +12,27 @@ module.exports = ({ env }) => ({
         database: env('DATABASE_NAME', 'wongames'),
         username: env('DATABASE_USERNAME', 'wongames'),
         password: env('DATABASE_PASSWORD', 'wongames'),
-        // ssl: env.bool('DATABASE_SSL', false),
+        ssl: env.bool('DATABASE_SSL', false),
       },
       options: {},
+    },
+    production: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'wongames'),
+        username: env('DATABASE_USERNAME', 'wongames'),
+        password: env('DATABASE_PASSWORD', 'wongames'),
+        schema: env('DATABASE_SCHEMA', 'public'),
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+        },
+      },
+      options: {
+        ssl: env.bool('DATABASE_SSL', false),
+      },
     },
   },
 });

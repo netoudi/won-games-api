8496119181766b90e25aaebb66ff497f592ac700
netoudi <netoudi@rocketmail.com>
2021-04-20 10:16:59 -0300

    Add Custom Controllers Wishlist

--- api/wishlist/controllers/wishlist.js
+++ api/wishlist/controllers/wishlist.js
@@ -1,8 +1,24 @@
 'use strict';
 
 /**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * Read the documentation
+ * (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+  async create(ctx) {
+    const token = await strapi.plugins['users-permissions'].services.jwt.getToken(ctx);
+
+    const body = {
+      ...ctx.request.body,
+      user: token.id,
+    };
+
+    const entity = await strapi.services.wishlist.create(body);
+
+    return sanitizeEntity(entity, { model: strapi.models.wishlist });
+  },
+};

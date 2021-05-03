681ca7175539e3577877bb64d889546f8490b322
netoudi <netoudi@rocketmail.com>
2021-05-03 09:25:58 -0300

    Search Cart Game info

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,12 +1,29 @@
 'use strict';
 
 /**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * Read the documentation
+ * (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
 module.exports = {
-  createPaymentIntent: () => {
-    return "Hello World!";
-  }
+  createPaymentIntent: async (ctx) => {
+    const { cart } = ctx.request.body;
+
+    let games = [];
+
+    await Promise.all(
+      cart?.map(async (game) => {
+        const validateGame = await strapi.services.game.findOne({
+          id: game.id,
+        });
+
+        if (validateGame) {
+          games.push(validateGame);
+        }
+      }),
+    );
+
+    return games;
+  },
 };
